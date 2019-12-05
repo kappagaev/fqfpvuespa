9013a89b8fc50422f8f15a3201e84614835155ed
kappagaev <kapagaev111@gmail.com>
2019-12-05 17:54:58 +0300

    vue-resource removed

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueResource  from 'vue-resource'
 
 Vue.config.productionTip = false
-
-Vue.use(VueResource);
 
 new Vue({
   router,
